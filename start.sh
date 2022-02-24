10a41491ab3ecf32d24cc1eb69dfae0f160ae98a
ramkch <99404492+ramkch@users.noreply.github.com>
2022-02-24 12:32:55 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ -z "${MESOS_SLAVE_PID}" ]]; then
     source ~/.chs_env/private_env
     source ~/.chs_env/global_env
-    source ~/.chs_env/company-links-consumer/env
+    source ~/.chs_env/company-profile-api/env
 
     PORT="${OFFICER_DELTA_PROCESSOR_PORT}"
 else
