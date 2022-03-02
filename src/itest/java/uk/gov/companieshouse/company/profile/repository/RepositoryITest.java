e043a2125f3bde6b1d3bca485f3842a6c3b38f4f
iichr <ciliev@companieshouse.gov.uk>
2022-03-02 18:07:09 +0000

    Refactor repository int test to use container annotation and rm redundant manual start/stop code

--- src/itest/java/uk/gov/companieshouse/company/profile/repository/RepositoryITest.java
+++ src/itest/java/uk/gov/companieshouse/company/profile/repository/RepositoryITest.java
@@ -1,8 +1,6 @@
 package uk.gov.companieshouse.company.profile.repository;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
@@ -10,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
@@ -20,22 +19,19 @@ class RepositoryITest {
 
     private static final String MOCK_COMPANY_NUMBER = "6146287";
 
-    static final MongoDBContainer mongoDBContainer = new MongoDBContainer(
+    // static, so container starts before the application context and we can set properties
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(
             DockerImageName.parse("mongo:4.0.10"));
-
-    @Autowired
-    private CompanyProfileRepository companyProfileRepository;
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-        mongoDBContainer.start();
     }
 
-    @BeforeAll
-    static void setup() {
-        mongoDBContainer.start();
-    }
+    @Autowired
+    private CompanyProfileRepository companyProfileRepository;
+
 
     @Test
     void should_return_mongodb_as_running() {
@@ -46,25 +42,20 @@ class RepositoryITest {
     void should_return_company_record_when_one_exists() {
         CompanyProfileApi companyProfile = new CompanyProfileApi();
         companyProfile.setCompanyNumber(MOCK_COMPANY_NUMBER);
-        companyProfileRepository.save(companyProfile);
+        this.companyProfileRepository.save(companyProfile);
 
         Assertions.assertTrue(
-                companyProfileRepository.findCompanyProfileApiByCompanyNumber(MOCK_COMPANY_NUMBER).isPresent());
+                this.companyProfileRepository.findCompanyProfileApiByCompanyNumber(MOCK_COMPANY_NUMBER).isPresent());
     }
 
     @Test
     void should_return_empty_optional_when_company_record_does_not_exist() {
         CompanyProfileApi companyProfile = new CompanyProfileApi();
         companyProfile.setCompanyNumber("242424");
-        companyProfileRepository.save(companyProfile);
+        this.companyProfileRepository.save(companyProfile);
 
         Assertions.assertTrue(
-                companyProfileRepository.findCompanyProfileApiByCompanyNumber("othernumber").isEmpty());
-    }
-
-    @AfterAll
-    static void tear() {
-        mongoDBContainer.stop();
+                this.companyProfileRepository.findCompanyProfileApiByCompanyNumber("othernumber").isEmpty());
     }
 
 }
