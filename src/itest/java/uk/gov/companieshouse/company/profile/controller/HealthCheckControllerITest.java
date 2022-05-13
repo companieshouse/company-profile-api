8ff6d5056adb682798619e40adb1a90a75d90724
sameerinfo2003 <sameer.info2003@gmail.com>
2022-05-13 13:34:20 +0100

    DSND-637: Expose GET /healthcheck endpoint using Spring actuator in Company-profile-api

--- src/itest/java/uk/gov/companieshouse/company/profile/controller/HealthCheckControllerITest.java
+++ src/itest/java/uk/gov/companieshouse/company/profile/controller/HealthCheckControllerITest.java
@@ -20,6 +20,6 @@ class HealthCheckControllerITest {
     @Test
     @DisplayName("Successfully returns health status")
     void returnHealthStatusSuccessfully() throws Exception {
-        mockMvc.perform(get("/healthcheck")).andExpect(status().isOk());
+        mockMvc.perform(get("/company-profile-api/healthcheck")).andExpect(status().isOk());
     }
 }
