c18a111455b70b3717a1d82a4f368898b74d3d0b
zahidm3 <zmuhammad@companieshouse.gov.uk>
2022-06-16 15:40:08 +0100

    Improving iTest performance

--- src/itest/java/uk/gov/companieshouse/company/profile/CucumberFeaturesRunnerITest.java
+++ src/itest/java/uk/gov/companieshouse/company/profile/CucumberFeaturesRunnerITest.java
@@ -5,14 +5,11 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
 import uk.gov.companieshouse.company.profile.configuration.AbstractIntegrationTest;
-import uk.gov.companieshouse.company.profile.configuration.CucumberSpringConfiguration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/itest/resources/features",
-        plugin = {"pretty",
-                "json:target/cucumber-report.json"
-        })
+        plugin = {"pretty", "json:target/cucumber-report.json"})
 @CucumberContextConfiguration
 public class CucumberFeaturesRunnerITest extends AbstractIntegrationTest {
 }
--- src/itest/java/uk/gov/companieshouse/company/profile/configuration/AbstractMongoConfig.java
+++ src/itest/java/uk/gov/companieshouse/company/profile/configuration/AbstractMongoConfig.java
@@ -1,9 +1,14 @@
 package uk.gov.companieshouse.company.profile.configuration;
 
+import java.time.Duration;
+
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * Mongodb configuration runs on test container.
@@ -15,7 +20,11 @@ public class AbstractMongoConfig {
 
     @DynamicPropertySource
     public static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        mongoDBContainer.setWaitStrategy(Wait.defaultWaitStrategy()
+                .withStartupTimeout(Duration.of(300, SECONDS)));
+        registry.add("spring.data.mongodb.uri", (() -> mongoDBContainer.getReplicaSetUrl() +
+                "?serverSelectionTimeoutMS=100&connectTimeoutMS=100"));
+
         mongoDBContainer.start();
     }
 }
