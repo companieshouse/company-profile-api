dfd7ddf9f10b9351e9b8a7f10bee6ed4c6ba6b2e
SALMANCH007 <salhadad@companieshouse.gov.uk>
2023-06-30 14:44:20 +0100

    Controller test

--- src/test/java/uk/gov/companieshouse/company/profile/controller/CompanyProfileControllerTest.java
+++ src/test/java/uk/gov/companieshouse/company/profile/controller/CompanyProfileControllerTest.java
@@ -29,10 +29,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import uk.gov.companieshouse.api.company.CompanyProfile;
 import uk.gov.companieshouse.api.company.Data;
@@ -700,19 +703,22 @@ class CompanyProfileControllerTest {
     }
 
     @Test
-    void testSearchCompanyProfile() throws JsonProcessingException, ResourceNotFoundException, com.fasterxml.jackson.core.JsonProcessingException {
+    void testSearchCompanyProfile() throws Exception {
         String companyNumber = "786";
         Data mockData = new Data();
+        String expectedResponseBody = "{\"property\": \"value\"}";
+
         ResponseEntity<Data> expectedResponse = new ResponseEntity<>(mockData, HttpStatus.OK);
 
         when(companyProfileService.retrieveCompanyNumber(companyNumber)).thenReturn(mockData);
 
-        ResponseEntity<Data> actualResponse = companyProfileController.searchComapnyProfile(companyNumber);
+        mockMvc.perform(MockMvcRequestBuilders.get("/company/{company_number}", companyNumber)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"property\": \"value\"}"))
+                .andExpect(MockMvcResultMatchers.model().attribute("response", expectedResponse));
 
-        assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
-        assertEquals(expectedResponse.getBody(), actualResponse.getBody());
         verify(companyProfileService, times(1)).retrieveCompanyNumber(companyNumber);
-
 
     }
 }
