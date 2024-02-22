2b7154490a6ebb52973230e56f88603b50ed9475
SALMANCH007 <salhadad@companieshouse.gov.uk>
2024-02-22 13:26:40 +0000

    changed routes

--- src/main/java/uk/gov/companieshouse/company/profile/controller/CompanyProfileController.java
+++ src/main/java/uk/gov/companieshouse/company/profile/controller/CompanyProfileController.java
@@ -134,7 +134,7 @@ public class CompanyProfileController {
      * @param companyNumber The number of the company
      * @return data object
      */
-    @GetMapping("/company/{company_number}")
+    @GetMapping("/company/test/{company_number}")
     public ResponseEntity<Data> searchCompanyProfile(
             @PathVariable("company_number") String companyNumber)
             throws JsonProcessingException, ResourceNotFoundException {
