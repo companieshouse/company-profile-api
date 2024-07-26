0684cbd957b4ec420bae110403b664acbdf01ac8
NeilShadrach <neil.shadrach@corryn.com>
2024-07-26 10:29:49 +0100

    SEC-47 CORS
    SEC-47 Add CORS tests

--- src/main/java/uk/gov/companieshouse/company/profile/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/company/profile/config/ApplicationConfig.java
@@ -7,20 +7,24 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 import uk.gov.companieshouse.api.company.Data;
 import uk.gov.companieshouse.api.converter.EnumWriteConverter;
+import uk.gov.companieshouse.api.filter.CustomCorsFilter;
 import uk.gov.companieshouse.company.profile.auth.EricTokenAuthenticationFilter;
 import uk.gov.companieshouse.company.profile.converter.CompanyProfileDataReadConverter;
 import uk.gov.companieshouse.company.profile.converter.CompanyProfileDataWriteConverter;
@@ -51,6 +55,8 @@ public class ApplicationConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable()
                 //REST APIs not enabled for cross site script headers
                 .csrf().disable() //NOSONAR
+                .cors().disable()
+                .addFilterBefore(new CustomCorsFilter(externalMethods()), CsrfFilter.class)
                 .formLogin().disable()
                 .logout().disable()
                 .sessionManagement()
@@ -101,6 +107,11 @@ public class ApplicationConfig extends WebSecurityConfigurerAdapter {
         objectMapper.registerModule(module);
 
         return objectMapper;
+    }
+
+    @Bean
+    public List<String> externalMethods() {
+        return Arrays.asList(HttpMethod.GET.name());
     }
 
 }
