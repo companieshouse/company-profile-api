317c82d77654d324155f2016230f6b40beab8c43
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-12-03 13:47:35 +0000

    BI-13957: Round published_at to seconds

--- src/main/java/uk/gov/companieshouse/company/profile/util/DateUtils.java
+++ src/main/java/uk/gov/companieshouse/company/profile/util/DateUtils.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.company.profile.util;
 
 import static java.time.ZoneOffset.UTC;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,5 +20,10 @@ public final class DateUtils {
                 (existingDeltaAt != null &&
                         OffsetDateTime.parse(requestDeltaAt, FORMATTER)
                                 .isBefore(existingDeltaAt.atOffset(UTC)));
+    }
+
+    public static String publishedAtString(final Instant source) {
+        return source.atOffset(UTC)
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss"));
     }
 }
